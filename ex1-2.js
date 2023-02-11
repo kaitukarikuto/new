ce0025d4e27875bcdda13bd6ce416ef5ef3258ea
kaitukarikuto <103973951+kaitukarikuto@users.noreply.github.com>
2023-02-11 14:58:51 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,13 +1,8 @@
-function nazo5(){
-if(document.getElementById('answer5').value=='イブラヒモビッチ'){
+function nazo1(){
+if(document.getElementById('answer1').value=='イブラヒモビッチ'){
  alert('あたり！');
 }
-else if(document.getElementById('answer5').value=='いぶらひもびっち'){
- alert('あたり！');
-}
-else if(document.getElementById('answer5').value=='ズラタンイブラヒモビッチ'){
- alert('あたり！');
-else if(document.getElementById('answer5').value=='ずらたんいぶらひもびっち'){
+else if(document.getElementById('answer1').value=='ズラタンイブラヒモビッチ'){
  alert('あたり！');
 else{
  alert('はずれ!');
