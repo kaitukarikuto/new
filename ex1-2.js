2a70b25133e2577294985ff1a8e8ae3bd12ae714
kaitukarikuto <103973951+kaitukarikuto@users.noreply.github.com>
2023-02-11 14:53:13 +0900

    Create ex1-2.js

---
+++ ex1-2.js
@@ -0,0 +1,15 @@
+function nazo5(){
+if(document.getElementById('answer5').value=='イブラヒモビッチ'){
+ alert('あたり！');
+}
+else if(document.getElementById('answer5').value=='いぶらひもびっち'){
+ alert('あたり！');
+}
+else if(document.getElementById('answer5').value=='ズラタンイブラヒモビッチ'){
+ alert('あたり！');
+else if(document.getElementById('answer5').value=='ずらたんいぶらひもびっち'){
+ alert('あたり！');
+else{
+ alert('はずれ!');
+ }
+}
